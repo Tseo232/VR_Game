ad1bfc8776341df56c8e38d87b7a8aeab2afbca5
GitDaigo <notzen87@gmail.com>
2025-06-09 01:15:06 +0200

    Keypad
    Working On Keypad Door.
    almost Done

--- VR_Game/Assets/Scripts_Hakimi/Keypad/Keypad.cs
+++ VR_Game/Assets/Scripts_Hakimi/Keypad/Keypad.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -11,6 +10,7 @@ namespace NavKeypad
         [Header("Events")]
         [SerializeField] private UnityEvent onAccessGranted;
         [SerializeField] private UnityEvent onAccessDenied;
+
         [Header("Combination Code (9 Numbers Max)")]
         [SerializeField] private int keypadCombo = 12345;
 
@@ -25,19 +25,21 @@ namespace NavKeypad
         [SerializeField] private float displayResultTime = 1f;
         [Range(0, 5)]
         [SerializeField] private float screenIntensity = 2.5f;
+
         [Header("Colors")]
-        [SerializeField] private Color screenNormalColor = new Color(0.98f, 0.50f, 0.032f, 1f); //orangy
-        [SerializeField] private Color screenDeniedColor = new Color(1f, 0f, 0f, 1f); //red
-        [SerializeField] private Color screenGrantedColor = new Color(0f, 0.62f, 0.07f); //greenish
+        [SerializeField] private Color screenNormalColor = new Color(0.98f, 0.50f, 0.032f, 1f);
+        [SerializeField] private Color screenDeniedColor = Color.red;
+        [SerializeField] private Color screenGrantedColor = Color.green;
+
         [Header("SoundFx")]
         [SerializeField] private AudioClip buttonClickedSfx;
         [SerializeField] private AudioClip accessDeniedSfx;
         [SerializeField] private AudioClip accessGrantedSfx;
+
         [Header("Component References")]
         [SerializeField] private Renderer panelMesh;
         [SerializeField] private TMP_Text keypadDisplayText;
         [SerializeField] private AudioSource audioSource;
-
 
         private string currentInput;
         private bool displayingResult = false;
@@ -49,46 +51,35 @@ namespace NavKeypad
             panelMesh.material.SetVector("_EmissionColor", screenNormalColor * screenIntensity);
         }
 
-
-        //Gets value from pressedbutton
         public void AddInput(string input)
         {
             audioSource.PlayOneShot(buttonClickedSfx);
             if (displayingResult || accessWasGranted) return;
-            switch (input)
-            {
-                case "enter":
-                    CheckCombo();
-                    break;
-                default:
-                    if (currentInput != null && currentInput.Length == 9) // 9 max passcode size 
-                    {
-                        return;
-                    }
-                    currentInput += input;
-                    keypadDisplayText.text = currentInput;
-                    break;
-            }
 
+            if (input == "enter")
+            {
+                CheckCombo();
+            }
+            else
+            {
+                if (currentInput.Length >= 9) return;
+                currentInput += input;
+                keypadDisplayText.text = currentInput;
+            }
         }
+
         public void CheckCombo()
         {
             if (int.TryParse(currentInput, out var currentKombo))
             {
-                bool granted = currentKombo == keypadCombo;
-                if (!displayingResult)
-                {
-                    StartCoroutine(DisplayResultRoutine(granted));
-                }
+                StartCoroutine(DisplayResultRoutine(currentKombo == keypadCombo));
             }
             else
             {
-                Debug.LogWarning("Couldn't process input for some reason..");
+                Debug.LogWarning("Invalid input");
             }
-
         }
 
-        //mainly for animations 
         private IEnumerator DisplayResultRoutine(bool granted)
         {
             displayingResult = true;
@@ -98,10 +89,11 @@ namespace NavKeypad
 
             yield return new WaitForSeconds(displayResultTime);
             displayingResult = false;
+
             if (granted) yield break;
+
             ClearInput();
             panelMesh.material.SetVector("_EmissionColor", screenNormalColor * screenIntensity);
-
         }
 
         private void AccessDenied()
@@ -126,6 +118,5 @@ namespace NavKeypad
             panelMesh.material.SetVector("_EmissionColor", screenGrantedColor * screenIntensity);
             audioSource.PlayOneShot(accessGrantedSfx);
         }
-
     }
 }
--- VR_Game/Assets/Scripts_Hakimi/Keypad/KeypadButton.cs
+++ VR_Game/Assets/Scripts_Hakimi/Keypad/KeypadButton.cs
@@ -1,64 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
 namespace NavKeypad
 {
+    [RequireComponent(typeof(XRSimpleInteractable))]
     public class KeypadButton : MonoBehaviour
     {
         [Header("Value")]
         [SerializeField] private string value;
+
         [Header("Button Animation Settings")]
         [SerializeField] private float bttnspeed = 0.1f;
         [SerializeField] private float moveDist = 0.0025f;
         [SerializeField] private float buttonPressedTime = 0.1f;
+
         [Header("Component References")]
         [SerializeField] private Keypad keypad;
 
+        private bool moving;
 
-        public void PressButton()
+        private void Awake()
+        {
+            var interactable = GetComponent<XRSimpleInteractable>();
+            interactable.selectEntered.AddListener(OnPressed);
+        }
+
+        private void OnPressed(SelectEnterEventArgs args)
         {
             if (!moving)
             {
                 keypad.AddInput(value);
                 StartCoroutine(MoveSmooth());
+
+                // Optional: Haptic feedback
+                if (args.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInputInteractor controller)
+                {
+                    controller.SendHapticImpulse(0.5f, 0.1f);
+                }
             }
         }
-        private bool moving;
 
         private IEnumerator MoveSmooth()
         {
-
             moving = true;
+
             Vector3 startPos = transform.localPosition;
-            Vector3 endPos = transform.localPosition + new Vector3(0, 0, moveDist);
+            Vector3 endPos = startPos + new Vector3(0, 0, moveDist);
 
             float elapsedTime = 0;
             while (elapsedTime < bttnspeed)
             {
                 elapsedTime += Time.deltaTime;
-                float t = Mathf.Clamp01(elapsedTime / bttnspeed);
-
-                transform.localPosition = Vector3.Lerp(startPos, endPos, t);
-
+                transform.localPosition = Vector3.Lerp(startPos, endPos, elapsedTime / bttnspeed);
                 yield return null;
             }
+
             transform.localPosition = endPos;
             yield return new WaitForSeconds(buttonPressedTime);
-            startPos = transform.localPosition;
-            endPos = transform.localPosition - new Vector3(0, 0, moveDist);
 
             elapsedTime = 0;
             while (elapsedTime < bttnspeed)
             {
                 elapsedTime += Time.deltaTime;
-                float t = Mathf.Clamp01(elapsedTime / bttnspeed);
-
-                transform.localPosition = Vector3.Lerp(startPos, endPos, t);
-
+                transform.localPosition = Vector3.Lerp(endPos, startPos, elapsedTime / bttnspeed);
                 yield return null;
             }
-            transform.localPosition = endPos;
 
+            transform.localPosition = startPos;
             moving = false;
         }
     }
