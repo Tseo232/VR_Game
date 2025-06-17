653d1dbd7f21ab548505513df9dce438bb4587b7
Tseo232 <113905825+Tseo232@users.noreply.github.com>
2025-06-17 14:07:16 +0200

    start scene
    lighting fix

---
+++ VR_Game/Assets/Tee/level transition.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelTransition : MonoBehaviour
+{
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject controlsPanel;
+
+    // This function shows the main menu and hides the controls
+    public void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        controlsPanel.SetActive(false);
+    }
+
+    // This function shows the controls panel and hides the main menu
+    public void ShowControls()
+    {
+        controlsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    // This function loads the next scene in the build settings
+    public void LoadNextLevel()
+    {
+       
+      SceneManager.LoadScene("Final");
+        
+        
+    }
+}
